fc42c3e596adfa9e7b4e33492395748df0615afb
xsling <me@xsl.sh>
2021-12-19 15:46:06 +0800

    initial modify

--- diy.sh
+++ diy.sh
@@ -1,5 +1,5 @@
 echo '修改机器名称'
-sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Xsling/g' package/base-files/files/bin/config_generate
 
 echo '修改网关地址'
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
@@ -11,17 +11,17 @@ echo '修改默认主题'
 sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
 
 #echo '去除默认bootstrap主题'
-#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 
 #echo '删除旧版argon,链接新版'
-#rm -rf ./package/lean/luci-theme-argon
-#git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon ../diy/luci-theme-argon
-#git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config ../diy/luci-app-argon-config
-#ln -s ../../../luci-theme-argon ./package/lean/
+rm -rf ./package/lean/luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon ../diy/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config ../diy/luci-app-argon-config
+ln -s ../../../luci-theme-argon ./package/lean/
 
 echo '修改wifi名称'
-sed -i 's/OpenWrt/G-DOCK/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/OpenWrt/Xsling/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 echo '修改banner'
 rm -rf package/base-files/files/etc/banner
@@ -35,6 +35,9 @@ echo '下载AdGuard Home'
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome ../diy/luci-app-adguardhome
 svn co https://github.com/kenzok8/openwrt-packages/trunk/adguardhome ../diy/adguardhome
 
+echo 'Download OpenClash'
+git clone https://github.com/vernesong/OpenClash ../diy/luci-app-openclash
+
 #echo 'JD script'
 #git clone https://github.com/Cathgao/luci-app-jd-dailybonus ../diy/luci-app-jd-dailybonus
 
@@ -42,7 +45,9 @@ echo '集成diy目录'
 ln -s ../../diy ./package/openwrt-packages
 
 #echo '首页增加CPU频率动态显示'
-#cp -f ../diy/mod-index.htm ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+cp -f ../diy/mod-index.htm ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
 #echo 'enable magic'
 #echo 'src-git helloworld https://github.com/fw876/helloworld'>>./feeds.conf.default
+
+
